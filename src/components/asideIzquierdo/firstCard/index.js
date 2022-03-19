75bf127d6b8bcae8a55599516f59b79a3a3a56ad
gustengo <frodriguezgustavo@gmail.com>
2022-03-19 12:08:27 -0600

    Creacion del contedido

--- src/components/asideIzquierdo/firstCard/index.js
+++ src/components/asideIzquierdo/firstCard/index.js
@@ -5,21 +5,25 @@ import "./card.scss";
 
 function cardCommunity() {
   return (
-    <Card id="community" style={{ width: "16rem" }}>
-      <Card.Body>
-        <Card.Title>
-         <span>DEV Community</span> is a community of 816,943 amazing developers{" "}
-        </Card.Title>
-        <Card.Text>
-          We're a place where coders share, stay up-to-date and grow their
-          careers.
-        </Card.Text>
-        <div>
-          <Button variant="outline-light">Go somewhere</Button>
-          <Button variant="outline-primary">Go somewhere</Button>
-        </div>
-      </Card.Body>
-    </Card>
+    <>
+      <Card id="community" style={{ width: "16rem" }}>
+        <Card.Body>
+          <Card.Title>
+            <span>DEV Community</span> is a community of 816,943 amazing
+            developers{" "}
+          </Card.Title>
+          <Card.Text>
+            We're a place where coders share, stay up-to-date and grow their
+            careers.
+          </Card.Text>
+          <div>
+            <Button variant="outline-light">Go somewhere</Button>
+            <Button variant="outline-primary">Go somewhere</Button>
+          </div>
+        </Card.Body>
+      </Card>
+      <br />
+    </>
   );
 }
 
--- src/components/asideIzquierdo/index.js
+++ src/components/asideIzquierdo/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import CardCommunity from './firstCard/index'
+import Contenido from './contenido/index'
 
 function asideIzquierdo(){
   return(
     <>
       <br />
       <CardCommunity />
+      <Contenido />
     </>
     
   );
