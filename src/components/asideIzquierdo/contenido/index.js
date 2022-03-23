fadd7359f203111b3f00d80fb5c57f3cd873201e
gustengo <frodriguezgustavo@gmail.com>
2022-03-22 21:02:34 -0600

    Mejorando los estilos

--- src/components/asideIzquierdo/contenido/index.js
+++ src/components/asideIzquierdo/contenido/index.js
@@ -1,14 +1,16 @@
 import React from "react";
 import contents from "./content.json";
 import Button from "react-bootstrap/Button";
+import "./contenido.scss"
 
 function buttonContent(props) {
   return contents.map((content) => {
     const { image, title } = content;
     return (
       <div id="contenido">
-        
-        <Button>{title}</Button>
+        <Button id="btn">
+          <img src="{image}" /> {title}
+        </Button>
       </div>
     );
   });
