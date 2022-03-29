a87298b9d142e1b056054e5fcfd4203bc53bccad
Erick-Gitbash <licerickps@gmail.com>
2022-03-28 23:13:22 -0600

    Vista login mejorada

--- src/components/LogIn/LoginCard.js
+++ src/components/LogIn/LoginCard.js
@@ -2,6 +2,9 @@ import React from "react";
 import Form from 'react-bootstrap/Form'
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
+import { DiApple } from "react-icons/di";
+import { BsGithub, BsTwitter } from "react-icons/bs";
+
 
 function LoginCardContent() {
     //Crear Endpoints para conectar con view index.js
@@ -13,15 +16,26 @@ function LoginCardContent() {
                         <Card.Title> Welcome to DEV Community </Card.Title>
                     </div>
                     <Card.Text> DEV Community is a community of 820,186 amazing developers </Card.Text>
+                    <div className="d-grid gap-2">
+                        <Button variant="dark" size="lg">
+                          <DiApple/>  Continue with Apple
+                        </Button>
+                        <Button variant="secondary" size="lg">
+                            Continue with Forem
+                        </Button>
+                        <Button variant="dark" size="lg">
+                           <BsGithub/> Continue with GitHub
+                        </Button>
+                        <Button variant="secondary" size="lg">
+                           <BsTwitter/> Continue with Twitter
+                        </Button>
+                    </div>
                     <Card.Text> Have a password? Continue with your email address </Card.Text>
 
                     <Form>
                         <Form.Group className="mb-3" controlId="formBasicEmail">
                             <Form.Label>Email address</Form.Label>
                             <Form.Control type="email" placeholder="Enter email" />
-                            <Form.Text className="text-muted">
-                                We'll never share your email with anyone else.
-                            </Form.Text>
                         </Form.Group>
 
                         <Form.Group className="mb-3" controlId="formBasicPassword">
@@ -31,8 +45,8 @@ function LoginCardContent() {
                         <Form.Group className="mb-3" controlId="formBasicCheckbox">
                             <Form.Check type="checkbox" label="Remember Me" />
                         </Form.Group>
-                        <Button variant="primary" type="submit">
-                           Continue
+                        <Button variant="primary" type="submit" size="lg">
+                            Continue
                         </Button>
                     </Form>
                     <Card.Text> I forgot my password </Card.Text>
